198048dc3d5e095d855d4f3acb792a360744a2fa
zigzig731 <zilizigergo@gmail.com>
2018-05-15 03:00:49 +0200

    Update
    Wave Spawner

--- src/main/java/hu/inf/unideb/td/Main.java
+++ src/main/java/hu/inf/unideb/td/Main.java
@@ -20,6 +20,7 @@ import hu.inf.unideb.td.view.Renderer;
 import org.joml.Vector3f;
 import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -71,63 +72,65 @@ public class Main {
         glCullFace(GL_BACK);
         glEnable(GL_DEPTH_TEST);
         Loader loader = new Loader();
-        StaticShader shader=new StaticShader();
-        Renderer renderer=new Renderer(shader);
-        Camera camera=new Camera();
-        Path.addWaypoint(new Vector3f(6,1,-2));
-        Path.addWaypoint(new Vector3f(6,1,-10));
-        Path.addWaypoint(new Vector3f(14,1,-10));
-        Path.addWaypoint(new Vector3f(14,1,-6));
-        Path.addWaypoint(new Vector3f(22,1,-6));
-        Path.addWaypoint(new Vector3f(22,1,-26));
-        Path.addWaypoint(new Vector3f(4,1,-26));
-        Path.addWaypoint(new Vector3f(4,1,-34));
-        Path.addWaypoint(new Vector3f(12,1,-34));
-        Path.addWaypoint(new Vector3f(12,1,-42));
-        Path.addWaypoint(new Vector3f(38,1,-42));
-        Path.addWaypoint(new Vector3f(38,1,-14));
-        Path.addWaypoint(new Vector3f(50,1,-14));
-        Path.addWaypoint(new Vector3f(70,9,-14));
-        Path.addWaypoint(new Vector3f(78,9,-14));
-        Path.addWaypoint(new Vector3f(78,9,-6));
-        Path.addWaypoint(new Vector3f(94,9,-6));
-        Path.addWaypoint(new Vector3f(94,9,-22));
-        Path.addWaypoint(new Vector3f(94,9,-22));
+        StaticShader shader = new StaticShader();
+        Renderer renderer = new Renderer(shader);
+        Camera camera = new Camera();
+        Path.addWaypoint(new Vector3f(6, 1, -2));
+        Path.addWaypoint(new Vector3f(6, 1, -10));
+        Path.addWaypoint(new Vector3f(14, 1, -10));
+        Path.addWaypoint(new Vector3f(14, 1, -6));
+        Path.addWaypoint(new Vector3f(22, 1, -6));
+        Path.addWaypoint(new Vector3f(22, 1, -26));
+        Path.addWaypoint(new Vector3f(4, 1, -26));
+        Path.addWaypoint(new Vector3f(4, 1, -34));
+        Path.addWaypoint(new Vector3f(12, 1, -34));
+        Path.addWaypoint(new Vector3f(12, 1, -42));
+        Path.addWaypoint(new Vector3f(38, 1, -42));
+        Path.addWaypoint(new Vector3f(38, 1, -14));
+        Path.addWaypoint(new Vector3f(50, 1, -14));
+        Path.addWaypoint(new Vector3f(70, 9, -14));
+        Path.addWaypoint(new Vector3f(78, 9, -14));
+        Path.addWaypoint(new Vector3f(78, 9, -6));
+        Path.addWaypoint(new Vector3f(94, 9, -6));
+        Path.addWaypoint(new Vector3f(94, 9, -22));
+        Path.addWaypoint(new Vector3f(94, 9, -22));
 
         //=====================================================================================================
         Texture waypointDiffuse = new Texture(loader.loadTexture("Textures/red.png"));
         SetupScene.setup();
 
         MaterialManager.get("metal_waypoint").setDiffuse(waypointDiffuse);
-        Entity turret=new Entity("metal_base","muzzleflash", new Vector3f(0,2f,4),new Vector3f(0,20,0),1f);
-        Entity turreta=new Entity("metal_base","enemyHR", new Vector3f(0,2f,-10),new Vector3f(0,20,0),1f);
-        Entity turretgun=new Entity("metal_base","towergun", new Vector3f(0,-0.5f,-10),new Vector3f(0,90,0),1f);
-        Entity waypointEntity=new Entity("metal_waypoint","waypoint", new Vector3f(0,0f,0),new Vector3f(0,0,0),1f);
-        Entity terrain=new Entity("metal_base","terrainHR", new Vector3f(0,0,-15),new Vector3f(0,-90,0),0.5f);
-        Entity plane=new Entity("forest_base","plane", new Vector3f(0,0,0),new Vector3f(0,0,0),0.5f);
-        Entity road=new Entity("Asphalt_base","terrain_road", new Vector3f(0,0,0),new Vector3f( 0,0,0),0.5f);
-        Entity water=new Entity("water_base","water", new Vector3f(0,0,0),new Vector3f(0,0,0),0.5f);
-        Entity building=new Entity("metal_light_base","mapbuildings", new Vector3f(0,0,0),new Vector3f(0,0,0),0.5f);
-        Entity gridunit=new Entity("healthbar_base","gridunit", new Vector3f(0,2,0),new Vector3f(0,0,0),1f);
+        Entity turret = new Entity("metal_base", "muzzleflash", new Vector3f(0, 2f, 4), new Vector3f(0, 20, 0), 1f);
+        Entity turreta = new Entity("metal_base", "enemyHR", new Vector3f(0, 2f, -10), new Vector3f(0, 20, 0), 1f);
+        Entity turretgun = new Entity("metal_base", "towergun", new Vector3f(0, -0.5f, -10), new Vector3f(0, 90, 0), 1f);
+        Entity waypointEntity = new Entity("metal_waypoint", "waypoint", new Vector3f(0, 0f, 0), new Vector3f(0, 0, 0), 1f);
+        Entity terrain = new Entity("metal_base", "terrainHR", new Vector3f(0, 0, -15), new Vector3f(0, -90, 0), 0.5f);
+        Entity plane = new Entity("forest_base", "plane", new Vector3f(0, 0, 0), new Vector3f(0, 0, 0), 0.5f);
+        Entity road = new Entity("Asphalt_base", "terrain_road", new Vector3f(0, 0, 0), new Vector3f(0, 0, 0), 0.5f);
+        Entity water = new Entity("water_base", "water", new Vector3f(0, 0, 0), new Vector3f(0, 0, 0), 0.5f);
+        Entity building = new Entity("metal_light_base", "mapbuildings", new Vector3f(0, 0, 0), new Vector3f(0, 0, 0), 0.5f);
+        Entity gridunit = new Entity("healthbar_base", "gridunit", new Vector3f(0, 2, 0), new Vector3f(0, 0, 0), 1f);
         //=====================================================================================================
         glClearColor(0.7f, 0.7f, 1f, 0.0f);
         glEnable(GL_NORMALIZE);
         Enemy bloodbunny = new Enemy();
-        List<Enemy> enemies=new ArrayList<Enemy>();
+        List<Enemy> enemies = new ArrayList<Enemy>();
         enemies.add(bloodbunny);
-        float lasttime=0;
+        float lasttime = 0;
 
         Session session = loader.loadSession("Session1");
         System.out.println(session.getWaves().get(0).getWaveComponents().get(1).getType());
         glEnable(GL_BLEND);
-        glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-        MousePicker picker = new MousePicker(camera,renderer.getProjectionmatrix());
+        MousePicker picker = new MousePicker(camera, renderer.getProjectionmatrix());
 
-        Integer currentEnemy=2;
-        Integer currentEnemyAmount=5;
-        Integer alreadySpawned=0;
-        Integer currentWaveComponent=-1;
+        Integer currentEnemy = 2;
+        Integer currentEnemyAmount = 0;
+        Integer alreadySpawned = 0;
+        Integer currentWaveComponent = -1;
+        Integer currentWave = 0;
+        float currentSpawnRate=1;
 
 
         while (!glfwWindowShouldClose(window)) {
@@ -149,43 +152,51 @@ public class Main {
             renderer.render(gridunit);
             renderer.render(turret);
 
-            for(GameObject enemy:GameObjectManager.gameObjects)
-            {
+            for (GameObject enemy : GameObjectManager.gameObjects) {
                 enemy.display(renderer);
                 enemy.update();
             }
 
-            Path.displayPath(renderer,waypointEntity);
+            Path.displayPath(renderer, waypointEntity);
             shader.stop();
 
-            if(glfwGetTime()-lasttime>1) {
-              /*  if()
+            if (glfwGetTime() - lasttime > currentSpawnRate) {
+                if(alreadySpawned==currentEnemyAmount)
                 {
-                currentWaveComponent++;
-                alreadyspawned=0;
-                    currentEnemy = session.getWaves().get(1).getWaveComponents().get(1).getType();
-                    currentEnemyAmount=session.getWaves().get(1).getWaveComponents().get(1).getAmount();
-                }*/
-                Enemy current=null;
-                switch (currentEnemy)
-                {
-                    case 1 : {
-                        current = new Enemy_Slow();
-                    }break;
-                    case 2 : {
-                        current = new Enemy_Runner();
-                    }break;
+                    alreadySpawned++;
+                    currentSpawnRate=5;
+                    continue;
                 }
-                if(alreadySpawned<=currentEnemyAmount)
-                GameObjectManager.add(current);
+                if (alreadySpawned >= currentEnemyAmount && currentWaveComponent != session.getWaves().get(currentWave).getWaveComponents().size() - 1) {
+                    currentWaveComponent++;
+                    alreadySpawned = 0;
+                    currentEnemy = session.getWaves().get(currentWave).getWaveComponents().get(currentWaveComponent).getType();
+                    currentEnemyAmount = session.getWaves().get(currentWave).getWaveComponents().get(currentWaveComponent).getAmount();
+                    currentSpawnRate = session.getWaves().get(currentWave).getWaveComponents().get(currentWaveComponent).getTimeInterval();
+
+                }
+                Enemy current = null;
+                System.out.println(alreadySpawned + " " + currentEnemyAmount);
+                switch (currentEnemy) {
+                    case 1: {
+                        current = new Enemy_Slow();
+                    }
+                    break;
+                    case 2: {
+                        current = new Enemy_Runner();
+                    }
+                    break;
+                }
+                if (alreadySpawned < currentEnemyAmount)
+                    GameObjectManager.add(current);
                 alreadySpawned++;
-                lasttime=(float)glfwGetTime();
+                lasttime = (float) glfwGetTime();
             }
 
             glfwSwapBuffers(window);
             glfwPollEvents();
             picker.update();
-            gridunit.setPosition(new Vector3f((int)(((float)picker.get3DPoint().x)/4.0f)*4,2,(int)(((float)picker.get3DPoint().z)/4f)*4));
+            gridunit.setPosition(new Vector3f((int) (((float) picker.get3DPoint().x) / 4.0f) * 4, 2, (int) (((float) picker.get3DPoint().z) / 4f) * 4));
             GameObjectManager.clean();
         }
 
--- src/main/java/hu/inf/unideb/td/model/gameObjects/enemies/Enemy_Runner.java
+++ src/main/java/hu/inf/unideb/td/model/gameObjects/enemies/Enemy_Runner.java
@@ -7,7 +7,7 @@ import org.joml.Vector3f;
 public class Enemy_Runner extends Enemy {
     public Enemy_Runner()
     {
-            super.setSpeed(1);
+            super.setSpeed(2f);
             entities.add(new Entity("metal_base","enemy"));
             entities.get(1).setLocalRotation(new Vector3f(0,180,0));
     }
